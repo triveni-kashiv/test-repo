c292ee5d6578a9bd990028cb2676bc57964e1de5
Trisha <trisha1.kashiv@gmail.com>
2017-08-01 11:56:27 +0530

    initial comit for Maven Testing project

---
+++ Maven_Testing_Project/src/main/java/Testing_Project/Maven_Testing_Project/App.java
@@ -0,0 +1,13 @@
+package Testing_Project.Maven_Testing_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven_Testing_Project/src/test/java/Testing_Project/Maven_Testing_Project/TestProgram.java
@@ -0,0 +1,37 @@
+package Testing_Project.Maven_Testing_Project;
+
+import org.testng.annotations.Test;
+
+
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class TestProgram {
+	 
+	WebDriver driver;
+	 
+	 
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+		  System.setProperty("webdriver.gecko.driver", "C:\\selenium3.0.1\\geckodriver.exe");
+		  driver = new FirefoxDriver();
+		  driver.get("http://www.google.com");
+		  
+	  }
+
+	  @Test
+	  public void SignUpLink() {
+		  WebElement ele = driver.findElement(By.id("gb_70"));
+		  ele.click();
+	  }
+	  
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+}
